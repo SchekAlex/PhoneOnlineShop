47e827b23e79dbc83a8c0bfb484e8c7bcbad36da
Schek Alex <ofmb1953@gmail.com>
2020-07-16 18:20:33 +0300

    first commit

---
+++ src/test/java/com/example/phonesonlineshop/PhonesonlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phonesonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonesonlineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
